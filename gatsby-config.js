7ec838f4bc7bde1272569b78b59e3812e29c35c9
kevin <frotor15@westerdals.gmail.com>
2019-03-04 14:57:24 +0100

    removed preview plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,14 +40,6 @@ module.exports = {
         htmlSerializer: () => prismicHtmlSerializer
       }
     },
-    {
-      resolve: "gatsby-plugin-prismic-preview",
-      options: {
-        repositoryName: "gatsby-starter-prismic-123",
-        linkResolver: require("./src/linkResolver"),
-        path: "/preview"
-      }
-    },
     "gatsby-plugin-lodash",
     "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
